6e3e62bb3dbf79aeaed85fe98c6dda90afc72fd0
KestiNet <esakesti@gmail.com>
2024-08-13 23:49:31 +0100

    json reading works

--- ccParser/jParser.py
+++ ccParser/jParser.py
@@ -1,1 +1,17 @@
 import json
+import os
+
+file_paths = ['tests/step1/invalid.json', 'tests/step1/valid.json']
+
+all_data = []
+
+for file_path in file_paths:
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            all_data.append(data)
+    else:
+        print(f"File not found: {file_path}")
+
+for idx, data in enumerate(all_data):
+    print(f"Data from file {file_paths[idx]}: {data}")
